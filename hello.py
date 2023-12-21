c4ba05a4199571943225484c516ee3b1e702c7db
AkashKumaraB <154533413+AkashKumaraB@users.noreply.github.com>
2023-12-21 20:28:36 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,99 @@
+# # printing methods in python
+# print("hello world")#by default print statements takes new line 
+# print('hello','hi','pavan')
+# print('hello','hi','pavan',sep='#')#it will seprate the things with '#'
+# print('hello','hi','pavan',end='@')# it will add any charecter at the end and also it will by defaultly removes '\n' charecter
+# print("how are you")
+
+#how strings work in python and input in python
+# print("hello jhova how are you")
+# print("your's only son 'jesus'has been there with us")
+# print('yours only son "jesus"has been there with us')
+
+# var="your name is:"
+# name=input("enter your name: ")
+# print(var+" "+name)
+# print(var+""+name)
+
+# #escape sequaces
+
+
+# # \n it goes to the new line
+# print("hello ekkadunnav\nhello emaive hello")
+# # \" or \' if you want double or single quotes in between as many times same as outside curly braces at that time we use this
+# print("hello ekkadunnav\"hello emaive hello")
+# print('hello ekkadunnav\'hello "emaive" hello')
+
+# #if we use trple quotes it will consider as next line also string outherwise it will not consider
+# # it will print in seprate line
+# str1="""hello ekkadunnav
+# hello emaive hello"""
+# print(str1)
+
+# # it will print in single line using(\) this symbol
+# str2="""hello ekkadunnav\
+# hello emaive hello"""
+# print(str2)
+
+# #\t it will gives the one tab space between the charecter
+# print("hi pavan \t how are you ?")
+# #\b it will will removes one back spacespace 
+# print("hi pavan\b how are you ?")
+
+# # if want to remove the any special charecter meaning we use this
+# print("c:\\User\desktop\new\firstdemo.pdf")
+# print("c:\\User\\desktop\\new\\firstdemo.pdf")
+# # another way:by simply spacifying as "r" this is called as "raw string"
+# print(r"c:\User\desktop\new\firstdemo.pdf")
+
+
+#\a it will give you the beep sound//curently this is not working we have import
+# print("hi hello how \a are you")
+
+# \r carrage return:it will remove the first word and moves cursor to the first position
+# print("hello hi\r vanakam")
+
+# \v vertical space:it will add the vertcal space//curently this is not working we have import
+# print("hello \vhi")
+# output as:hello
+#                hi
+
+# \ooo:it will take the three numbers in octal and it will print in english
+# print("\145\116\102")
+
+#\xhh:it takes xand 2 numbers in hexa decial and it will print in english
+# print("\x23\x25\x48")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
